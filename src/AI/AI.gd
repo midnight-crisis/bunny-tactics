2411f62d539a37274c26308244718bc31615a895
Tim Montesa <me@tmontesa.com>
2022-05-21 19:45:46 -0700

    AI WIP

--- src/AI/AI.gd
+++ src/AI/AI.gd
@@ -10,13 +10,13 @@ const vM = Vector2(Global.MAP_TILES_WIDTH, Global.MAP_TILES_HEIGHT)
 func _ready() -> void:
 	pass
 	
-func control(unit, map):
+func control(unit, map, units):
 	# Start
 	# Update active unit, current map, current unit placement
 	# Find any enemies in attack range
 		# If so, attack 
 	# Calculate accessible tiles
-	# Find the closest enemy
+	# Find the closest enemy within reachable tiles
 		# If enemy is found, set any accessible tile around it as move target
 		# If no enemy is found, set random accessible move target (bad)
 	# If still not attacked, find any enemies in attack range
@@ -26,22 +26,58 @@ func control(unit, map):
 	
 	print("Starting AI for " + String(unit.species))
 	
+	var attack_tiles = get_reachable_tiles(unit.tile_position, unit.attack_reach)
+	var attack_target = pick_attack_target(attack_tiles)
+	if (attack_target != null):
+		pass # DAMAGE TARGET
 		
-	pass
+	var move_tiles = get_reachable_tiles(unit.tile_position, unit.move_reach)
+	var reachable_unit = pick_attack_target(move_tiles)
 	
+	if (reachable_unit):
+		var adjacent_tile = pick_accessible_adjacent_tile(move_tiles, reachable_unit.tile_position)
+		if (adjacent_tile):
+			pass # MOVE TO TILE
+			pass # DAMAGE TARGET
+			
+	else: # No unit in sight
+		var random_tile = move_tiles[randi() % move_tiles.size()]
+		if (random_tile):
+			pass # MOVE TO TILE
+		pass # WAIT FOR PASSIVE HEAL
+	
+	print("Ending AI")
+	
+func pick_accessible_adjacent_tile(tiles, pos):
+	var targets = [
+		Vector2(pos.x, pos.y + 1),
+		Vector2(pos.x + 1, pos.y),
+		Vector2(pos.x, pos.y - 1),
+		Vector2(pos.x - 1, pos.y)
+	]
+	
+	for a in targets:
+		if (tiles.has(a)):
+			return a
+	return null
+	
+func pick_attack_target(tiles):
+	for t in tiles:
+		var u = units[t.x][t.y]
+		if (u != null && u.team != unit.team):
+			return u
+	return null
+
 func get_reachable_tiles(pos, reach):
-	var tiles = _calcR(pos, reach)
+	var tiles = _calcTile(pos, reach)
 	
 	for i in range(0, tiles.size()):
 		for j in range(tiles.size() - 1, i):
 			if (tiles[i] == tiles[j]):
 				tiles.pop_at(j)
 	return tiles
-	
-func get_reachable_units():
-	pass
 
-func _calcR(pos, reach):
+func _calcTile(pos, reach):
 	if (reach == 0): return []
 	
 	var reachable = []
@@ -61,7 +97,7 @@ func _calcR(pos, reach):
 		|| (map[pos.x][pos.y] == Global.Tile.WATER && unit.can_traverse_water)
 		|| (map[pos.x][pos.y] == Global.Tile.FENCE && unit.can_traverse_fence))):
 			reachable.append(t)
-			reachable.append_array(_calcRT(t, reach - 1))
+			reachable.append_array(_calcTile(t, reach - 1))
 	
 	return reachable
 
