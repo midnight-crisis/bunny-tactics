dda5fd9d4eeb855fbf32cbafc6736b14ec48cac2
Tim Montesa <me@tmontesa.com>
2022-05-26 22:32:45 -0700

    Refactored Unit

--- src/Unit/Unit.gd
+++ src/Unit/Unit.gd
@@ -1,89 +1,120 @@
 extends Node2D
 class_name Unit
 
-signal unit_clicked
-signal unit_dead
+var first_name: String = "<Name>"
+var species: String = "<Species>"
+var job: String = "<Job>"
 
-var player_controllable = false
+var map_position: Vector2 = Vector2(-1, -1)
+var is_hole_traversing: bool = false
+var is_water_traversing: bool = false
+var is_fence_traversing: bool = false
 
-var fullname = "Unnamed"
-var species = "Human"
-var job = "Dummy"
-var team = Global.Team.NONE
+var max_health: int = 10
+var health: int = max_health
+var passive_heal: int = 0
+var attack: int = 1
 
-var special_action = Global.ActionType.SPECIAL_NONE
-var can_traverse_holes = false
-var can_traverse_water = false
-var can_traverse_fence = false
+var move_reach: int = 3
+var attack_reach: int = 1
+var special_reach: int = 2
+var special_action: int = Global.ActionType.SPECIAL_NONE
 
-var tile_position = Vector2(-1, -1)
-var health = 15
-var max_health = 30
-var attack = 1
-var passive_heal_amount = 1
+var team: int = Global.Team.NONE
+var has_moved: bool = false
+var has_acted: bool = false
 
-var move_reach = 2
-var attack_reach = 1
-var special_reach = 1
-
-var has_moved = false
-var has_acted = false
-
-onready var Name = $Name
-onready var UnitSprite = $Sprite
-onready var Arrow = $Arrow
-onready var HealthBar = $HealthBar
-onready var Tweener = $Tweener
-onready var UnitCamera = $Camera
-onready var ArrowAnimPlayer = $ArrowAnimPlayer
-onready var SpriteAnimPlayer = $SpriteAnimPlayer
-onready var UnitManager = get_parent()
+onready var UnitSprite = $UnitSprite
+onready var ArrowSprite = $ArrowSprite
+onready var InteractArea = $InteractArea
 
 func _ready() -> void:
-	Name.text = fullname
-	HealthBar.set_percentage(float(health) / float(max_health))
-	ArrowAnimPlayer.play("Float")
+	pass
+
+# ======================================
+# Position
+# ======================================
+
+func get_position() -> Vector2:
+	return map_position
+
+func set_position(pos: Vector2) -> void:
+	map_position = pos
+
+# ======================================
+# Health
+# ======================================
+
+func get_health() -> int:
+	return health
+
+func set_health(n: int) -> void:
+	health = int(clamp(n, 0, max_health))
 	
-func reset_flags():
+func get_max_health() -> int:
+	return max_health
+
+func set_max_health(n: int) -> void:
+	var health_difference = n - max_health
+	if (health_difference > 0):
+		heal(health_difference)
+	elif(health_difference < 0):
+		damage(health_difference)
+	
+	max_health = int(max(n, 1))
+
+# ======================================
+# Damage / Healing
+# ======================================
+
+func damage(n: int) -> void:
+	set_health(health - n)
+	
+func damage_percentage(k: float) -> void:
+	set_health(health * k)
+	
+func damage_by_unit(unit: Unit) -> void:
+	damage(unit.attack)
+
+func heal(n: int) -> void:
+	set_health(health + n)
+
+func heal_percentage(k: float) -> void:
+	set_health(health * (k + 1))
+	
+func passive_heal() -> void:
+	set_health(health + passive_heal)
+	
+# ======================================
+# Team
+# ======================================
+
+func get_team() -> int:
+	return team
+
+func set_team(t: int) -> void:
+	team = t
+
+# ======================================
+# Flag Options
+# ======================================
+
+func flag_movement() -> void:
+	has_moved = true
+
+func flag_action() -> void:
+	has_acted = true
+
+func reset_flags() -> void:
 	has_moved = false
 	has_acted = false
-	
-func hurt(n):
-	health = clamp(health - n, 0, max_health)
-	HealthBar.set_percentage(float(health) / float(max_health))
-	if (health <= 0):
-		emit_signal("unit_dead", self)
-		Global.PlayAudio("DEATH")
-	else:
-		Global.PlayAudio("OUCH")
-	
-func simulate_click():
-	emit_signal("unit_clicked", self)
-	
-func anim_attack():
-	SpriteAnimPlayer.play("attack")
-	Global.PlayAudio("ATTACK")
-	
-func anim_move():
-	SpriteAnimPlayer.play("move")
-	Global.PlayAudio("MOVE")
-	
-func anim_wait():
-	SpriteAnimPlayer.play("wait")
-	Global.PlayAudio("HOVER")
 
-func anim_special():
-	SpriteAnimPlayer.play("special")
+# ======================================
+# InteractArea
+# ======================================
 
 func _on_InteractArea_mouse_entered() -> void:
-	Name.visible = true
+	ArrowSprite.visble = true
 
 func _on_InteractArea_mouse_exited() -> void:
-	Name.visible = false
-
-func _on_InteractArea_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if (UnitManager.current_action != Global.ActionType.NONE):
-		print("nopeasdjkaskldj;lasd")
-		return
-	if (event is InputEventMouseButton && event.pressed && Input.is_action_pressed("click")):
-		emit_signal("unit_clicked", self)
+	ArrowSprite.visble = false
---
+++ src/Unit/Unit.old.gd
@@ -0,0 +1,89 @@
+extends Node2D
+class_name Unit_old
+
+signal unit_clicked
+signal unit_dead
+
+var player_controllable = false
+
+var fullname = "Unnamed"
+var species = "Human"
+var job = "Dummy"
+var team = Global.Team.NONE
+
+var special_action = Global.ActionType.SPECIAL_NONE
+var can_traverse_holes = false
+var can_traverse_water = false
+var can_traverse_fence = false
+
+var tile_position = Vector2(-1, -1)
+var health = 15
+var max_health = 30
+var attack = 1
+var passive_heal_amount = 1
+
+var move_reach = 2
+var attack_reach = 1
+var special_reach = 1
+
+var has_moved = false
+var has_acted = false
+
+onready var Name = $Name
+onready var UnitSprite = $Sprite
+onready var Arrow = $Arrow
+onready var HealthBar = $HealthBar
+onready var Tweener = $Tweener
+onready var UnitCamera = $Camera
+onready var ArrowAnimPlayer = $ArrowAnimPlayer
+onready var SpriteAnimPlayer = $SpriteAnimPlayer
+onready var UnitManager = get_parent()
+
+func _ready() -> void:
+	Name.text = fullname
+	HealthBar.set_percentage(float(health) / float(max_health))
+	ArrowAnimPlayer.play("Float")
+	
+func reset_flags():
+	has_moved = false
+	has_acted = false
+	
+func hurt(n):
+	health = clamp(health - n, 0, max_health)
+	HealthBar.set_percentage(float(health) / float(max_health))
+	if (health <= 0):
+		emit_signal("unit_dead", self)
+		Global.PlayAudio("DEATH")
+	else:
+		Global.PlayAudio("OUCH")
+	
+func simulate_click():
+	emit_signal("unit_clicked", self)
+	
+func anim_attack():
+	SpriteAnimPlayer.play("attack")
+	Global.PlayAudio("ATTACK")
+	
+func anim_move():
+	SpriteAnimPlayer.play("move")
+	Global.PlayAudio("MOVE")
+	
+func anim_wait():
+	SpriteAnimPlayer.play("wait")
+	Global.PlayAudio("HOVER")
+
+func anim_special():
+	SpriteAnimPlayer.play("special")
+
+func _on_InteractArea_mouse_entered() -> void:
+	Name.visible = true
+
+func _on_InteractArea_mouse_exited() -> void:
+	Name.visible = false
+
+func _on_InteractArea_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if (UnitManager.current_action != Global.ActionType.NONE):
+		print("nopeasdjkaskldj;lasd")
+		return
+	if (event is InputEventMouseButton && event.pressed && Input.is_action_pressed("click")):
+		emit_signal("unit_clicked", self)
