2b4739914b41003e7940fa424381aee9537fc1ec
Tim Montesa <me@tmontesa.com>
2022-05-31 21:39:42 -0700

    WIP Refactoring UpgradeMenu

--- src/UpgradeMenu/UpgradeMenu.gd
+++ src/UpgradeMenu/UpgradeMenu.gd
@@ -1,9 +1,6 @@
 extends Control
 
-signal upgrade_add_unit
-signal upgrade_heal
-
-enum Upgrades {
+enum Reward {
 	ATTACK,
 	MOVE_REACH,
 	ATTACK_REACH,
@@ -12,128 +9,49 @@ enum Upgrades {
 	HEAL
 }
 
-onready var upgrades = [
-	Upgrades.ATTACK,
-	Upgrades.MOVE_REACH,
-	Upgrades.ATTACK_REACH,
-	Upgrades.MEDIC_HEAL,
-	Upgrades.NEW_UNIT,
-	Upgrades.HEAL	
+onready var rewards = [
+	Reward.ATTACK,
+	Reward.MOVE_REACH,
+	Reward.ATTACK_REACH,
+	Reward.MEDIC_HEAL,
+	Reward.NEW_UNIT,
+	Reward.HEAL	
 ]
 
-onready var UpgradeIcon = {
-	Upgrades.ATTACK: preload("res://UpgradeMenu/assets/attack-up.png"),
-	Upgrades.MOVE_REACH: preload("res://UpgradeMenu/assets/move-up.png"),
-	Upgrades.ATTACK_REACH: preload("res://UpgradeMenu/assets/reach-up.png"),
-	Upgrades.MEDIC_HEAL: preload("res://UpgradeMenu/assets/medic-heal-up.png"),
-	Upgrades.NEW_UNIT: preload("res://UpgradeMenu/assets/new-normal-bun.png"),
-	Upgrades.HEAL: 	preload("res://UpgradeMenu/assets/heal.png")
+onready var RewardIcon = {
+	Reward.ATTACK: preload("res://UpgradeMenu/assets/attack-up.png"),
+	Reward.MOVE_REACH: preload("res://UpgradeMenu/assets/move-up.png"),
+	Reward.ATTACK_REACH: preload("res://UpgradeMenu/assets/reach-up.png"),
+	Reward.MEDIC_HEAL: preload("res://UpgradeMenu/assets/medic-heal-up.png"),
+	Reward.NEW_UNIT: preload("res://UpgradeMenu/assets/new-normal-bun.png"),
+	Reward.HEAL: preload("res://UpgradeMenu/assets/heal.png")
 }
 
-onready var DefaultTheme = preload("res://Theme/DefaultThemeBlackText.tres")
+onready var RewardDescription = {
+	Reward.ATTACK: "Ouch! Increase the attack of a bunny by 50%.",
+	Reward.MOVE_REACH: "Nyoom! Increase the movement range of any bunny by 1 tile.",
+	Reward.ATTACK_REACH: "Woah! Increase the attack range of any bunny by 1 tile.",
+	Reward.MEDIC_HEAL: "Ding! Increase the doctor bunny's heal by 75%.",
+	Reward.NEW_UNIT: "Ta-da! Add a new fighter bunny to your team.",
+	Reward.HEAL: "Phew! Heal all of your bunnies for up to 50% of their max health."
+}
 
-onready var RewardMenu = $Reward
-onready var UnitMenu = $Unit
-onready var UpgradeSprite1 = $Reward/CenterContainer/PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/UpgradeButton1/UpgradeSprite1
-onready var UpgradeSprite2 = $Reward/CenterContainer/PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/UpgradeButton2/UpgradeSprite2
-onready var UnitGrid = $Unit/CenterContainer2/PanelContainer/MarginContainer/VBoxContainer/GridContainer
+onready var RewardSelection = $RewardSelection
+onready var Rewards = $RewardSelection/Center/Panel/Container/Content/Rewards
+onready var RewardButtons = [
+	$RewardSelection/Center/Panel/Container/Content/Rewards/Reward1Button,
+	$RewardSelection/Center/Panel/Container/Content/Rewards/Reward2Button]
+onready var RewardIcons = [
+	$RewardSelection/Center/Panel/Container/Content/Rewards/Reward1Button/Reward1Icon,
+	$RewardSelection/Center/Panel/Container/Content/Rewards/Reward2Button/Reward2Icon]
+onready var Description = $RewardSelection/Center/Panel/Container/Content/Description
 
-var upgrade_1  = null
-var upgrade_2  = null
-var upgrade_selected = null
-var units = []
+onready var UnitSelection = $UnitSelection
+onready var UnitList = $UnitSelection/Center/Panel/Container/Content/UnitList
 
 func _ready() -> void:
 	pass
-
-func upgrade(friendly_units):
-	_switch_to_reward_menu()
-	units = friendly_units
 	
-	# Get 2 unique rewards
-	upgrade_1 = _get_random_upgrade()
-	upgrade_2 = upgrade_1
-	while (upgrade_2 == upgrade_1):
-		upgrade_2 = _get_random_upgrade()
-	
-	# Set icons for the upgrade buttons
-	UpgradeSprite1.texture = UpgradeIcon[upgrade_1]
-	UpgradeSprite2.texture = UpgradeIcon[upgrade_2]
-	
-func apply_upgrade():
-	if (upgrade_selected == Upgrades.NEW_UNIT):
-		emit_signal("upgrade_add_unit")
-		_close_upgrade_window()
-	elif (upgrade_selected == Upgrades.HEAL):
-		emit_signal("upgrade_heal")
-		_close_upgrade_window()
-	else: # The other upgrades are unit-dependent
-		unit_selection()
-
-func unit_selection():
-	_switch_to_unit_menu()
-	_clear_unit_grid()
-	
-	# Create a button for each (friendly) unit
-	for u in units:
-		if (u.job == "Doctor" || upgrade_selected != Upgrades.MEDIC_HEAL):
-			var unit_name = u.fullname
-			var unit_job = u.job
-			var button_text = unit_name + ": " + unit_job
-			_create_new_button(button_text, u)
-
-func apply_unit_upgrade(unit):
-	if (upgrade_selected == Upgrades.ATTACK):
-		unit.attack += 1
-	elif (upgrade_selected == Upgrades.MOVE_REACH):
-		unit.move_reach += 1
-	elif (upgrade_selected == Upgrades.ATTACK_REACH):
-		unit.attack_reach += 1
-	elif (upgrade_selected == Upgrades.MEDIC_HEAL):
-		unit.heal_amount = int(floor(unit.heal_amount * 1.5))
-	else:
-		print("Invalid unit upgrade?")
-	
-	_close_upgrade_window()
-	
-	
-func _on_unit_chosen(unit):
-	apply_unit_upgrade(unit)
-
-func _create_new_button(text, unit):
-	var button = Button.new()
-	UnitGrid.add_child(button)
-	button.theme = DefaultTheme
-	button.text = text
-	button.connect("pressed", self, "_on_unit_chosen", [unit])
-
-func _close_upgrade_window():
-	self.visible = false
-	RewardMenu.visible = false
-	UnitMenu.visible = false
-
-func _switch_to_reward_menu():
-	self.visible = true
-	RewardMenu.visible = true
-	UnitMenu.visible = false
-
-func _switch_to_unit_menu():
-	self.visible = true
-	RewardMenu.visible = false
-	UnitMenu.visible = true
-	
-func _get_random_upgrade():
-	return upgrades[Global.rng.randi() % upgrades.size()]
-	
-func _on_upgrade_1_selected():
-	upgrade_selected = upgrade_1
-	apply_upgrade()
-
-func _on_upgrade_2_selected():
-	upgrade_selected = upgrade_2
-	apply_upgrade()
-	
-func _clear_unit_grid():
-	for g in UnitGrid.get_children():
-		UnitGrid.remove_child(g)
-		g.queue_free()
+# ======================================
+# Reward Selection
+# ======================================
